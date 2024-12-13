e589725318e808b865a7a4d0b678f43e2145b2b3
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-12-14 01:57:30 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -6,11 +6,16 @@ vim.opt.runtimepath:append("~/.vim/after")
 vim.opt.packpath = vim.opt.runtimepath:get()
 vim.cmd("source ~/.vimrc")
 
+-- auto format on save by :w ,
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end
+})
+
 -- ~/.config/nvim/pack/nvim/start/nvim-lspconfig/lua/lspconfig/
 require'lspconfig'.clangd.setup{}
-
--- ~/.config/nvim/pack/nvim/start/nvim-treesitter/lua/nvim-treesitter/
-require'nvim-treesitter'.setup{}
 
 -- ~/.config/nvim/pack/nvim/start/aerial.nvim/lua/aerial/
 require("aerial").setup({
@@ -22,12 +27,19 @@ require("aerial").setup({
 -- leader key is backslash \ so use \a in normal mode
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
 
--- auto format
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
+-- ~/.config/nvim/pack/nvim/start/nvim-tree/lua/
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup({
+  view = {
+    width = 30,
+  },
 })
+
+vim.keymap.set("n", "<F2>", "<cmd>:NvimTreeToggle<CR>")
+
+-- ~/.config/nvim/pack/nvim/start/nvim-treesitter/lua/nvim-treesitter/
+-- require'nvim-treesitter'.setup{}
 
 --
