a634eb10e3682eb4f371b8ef7d271e98fbd4122d
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-12-18 22:13:26 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -27,17 +27,17 @@ require("aerial").setup({
 -- leader key is backslash \ so use \a in normal mode
 vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
 
--- ~/.config/nvim/pack/nvim/start/nvim-tree/lua/nvim-tree
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup({
-  view = {
-    width = 30,
-  },
-})
-vim.keymap.set("n", "<F2>", "<cmd>:NvimTreeToggle<CR>")
-
 -- ~/.config/nvim/pack/nvim/start/nvim-treesitter/lua/nvim-treesitter/
--- require'nvim-treesitter'.setup{}
+require'nvim-treesitter'.setup{}
+
+-- ~/.config/nvim/pack/nvim/start/nvim-tree/lua/nvim-tree
+--vim.g.loaded_netrw = 1
+--vim.g.loaded_netrwPlugin = 1
+--require("nvim-tree").setup({
+--  view = {
+--    width = 30,
+--  },
+--})
+--vim.keymap.set("n", "<F2>", "<cmd>:NvimTreeToggle<CR>")
 
 --
