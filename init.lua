5b993f96cb27c4dc9668850f1ca78f0041e89f6c
lijh8 <9011764+lijh8@users.noreply.github.com>
2024-12-24 03:18:45 +0800

    Add files via upload

--- init.lua
+++ init.lua
@@ -7,27 +7,27 @@ vim.opt.packpath = vim.opt.runtimepath:get()
 vim.cmd("source ~/.vimrc")
 
 -- auto format on save by :w ,
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function()
-    vim.lsp.buf.format({ async = false })
-  end
-})
+--vim.api.nvim_create_autocmd("BufWritePre", {
+--  pattern = "*",
+--  callback = function()
+--    vim.lsp.buf.format({ async = false })
+--  end
+--})
 
 -- ~/.config/nvim/pack/nvim/start/nvim-lspconfig/lua/lspconfig/
-require'lspconfig'.clangd.setup{}
+--require'lspconfig'.clangd.setup{}
 
 -- ~/.config/nvim/pack/nvim/start/aerial.nvim/lua/aerial/
-require("aerial").setup({
-  layout = {
-    width = 25,
-  },
-  autojump = true,
-})
+--require("aerial").setup({
+--  layout = {
+--    width = 25,
+--  },
+--  autojump = true,
+--})
 -- leader key is backslash \ so use \a in normal mode
-vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
+--vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle<CR>")
 
 -- ~/.config/nvim/pack/nvim/start/nvim-treesitter/lua/nvim-treesitter/
-require'nvim-treesitter'.setup{}
+--require'nvim-treesitter'.setup{}
 
 --
